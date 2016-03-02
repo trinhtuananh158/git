071c05d8c2ed919126f8060095e0b1ee347f6acf
Tuan Anh <trinhtuananh158@gmail.com>
2016-03-02 14:25:02 +0700

    Replace Third commit

--- untitled/src/Sample.java
+++ untitled/src/Sample.java
@@ -25,14 +25,7 @@ public class Sample {
     }
     static void showDir(int indent, File file)
             throws IOException {
-        for (int i = 0; i < indent; i++)
-            System.out.print('-');
-        System.out.println(file.getName());
-        if (file.isDirectory()) {
-            File[] files = file.listFiles();
-            for (int i = 0; i < files.length; i++)
-                showDir(indent + 4, files[i]);
-        }
+
     }
     public static boolean deleteDir(File dir) {
         if (dir.isDirectory()) {
